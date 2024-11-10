b7a62344e5ca985e9ca73457f71cc6bd1b5afc7a
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-11-11 01:40:10 +0900

    テストの共通部分をリファクタリングした

--- main_test.go
+++ main_test.go
@@ -5,19 +5,23 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Atsushi")
-	want := "Hello, Atsushi"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	asserCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Atsushi")
+		want := "Hello, Atsushi"
+		asserCorrectMessage(t, got, want)
+	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied",
 		func(t *testing.T) {
 			got := Hello("")
 			want := "Hello, World"
-			if got != want {
-				t.Errorf("got %q want %q", got, want)
-			}
+			asserCorrectMessage(t, got, want)
 		})
 }
