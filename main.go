c2352db7800205f0920795052130a6baa0060cfc
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-11-16 17:20:41 +0900

    フランス語での挨拶を追加

--- main.go
+++ main.go
@@ -5,14 +5,26 @@ import (
 )
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frechHelloPrefix = "Bonjour, "
+const french = "French"
+const spanish = "Spanish"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		return englishHelloPrefix + "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frechHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Atsushi"))
+	fmt.Println(Hello("Atsushi", "French"))
 }
--- main_test.go
+++ main_test.go
@@ -13,15 +13,21 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Atsushi")
+		got := Hello("Atsushi", "")
 		want := "Hello, Atsushi"
 		asserCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied",
 		func(t *testing.T) {
-			got := Hello("")
+			got := Hello("", "")
 			want := "Hello, World"
 			asserCorrectMessage(t, got, want)
 		})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		asserCorrectMessage(t, got, want)
+	})
 }
