719c6983de29dc9157468181172afe2333f067a9
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-11-16 17:31:49 +0900

    Hello関数からプレフィックスを切り出す

--- main.go
+++ main.go
@@ -16,15 +16,19 @@ func Hello(name string, language string) string {
 
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
